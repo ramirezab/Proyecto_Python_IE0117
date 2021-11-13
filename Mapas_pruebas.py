d224bca4f53d71fe29ff4debee0fb626b6eb18a9
ramirezab <bryanramirez1220@gmail.com>
2021-11-13 16:21:21 -0600

    Comentarios agregados

--- Mapas_pruebas.py
+++ Mapas_pruebas.py
@@ -11,17 +11,22 @@ def mapeo(image):
     que representa los pixeles negros de la imagen con la letra b
     """
 
-    im = Image.open(image)
+    im = Image.open(image) # Carga la imagen
     
-    im2 = im.resize((screen_size.current_h-150,screen_size.current_h-150))
+    # Cambia el tamaño de la imagen para que se ajuste a la
+    # pantalla del juego
+    im2 = im.resize((screen_size.current_h-150,screen_size.current_h-150)) 
     
+    # Crea una lista con todos los pixeles en RGB de a imagen
     pix = im2.load()
-    mapa=[]
-    x = 20
-    for row in range(0,im2.size[0],5):
+    mapa=[] # Lista que guarda la posicion de los ixeles relevantes de la imagen
+
+    x = 20 # Variacion aceptable de pixeles negres
+    for row in range(0,im2.size[0],5): # Revision de filas de pixeles de la imagen
         a=[]
-        for column in range(0,im2.size[1],5):
+        for column in range(0,im2.size[1],5): # Revision de columnas de pixeles de la imagen
             
+            # Comprobación de rango aceptable de tonos de negro
             if pix[row, column][0] in range(x) and pix[row, column][1] in range(x) and pix[row, column][2] in range(x):
                 
                 a.append("b")
@@ -30,7 +35,7 @@ def mapeo(image):
             
         
             
-        b = "".join(a)
+        b = "".join(a) # Definicion de str ue contiene 1 fila de pixeles de la imagen
         mapa.append(b) 
     
     return mapa
