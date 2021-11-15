c5a58e7669d9f5309131716287388945d650ea3b
Alexander <ap16012001@gmail.com>
2021-11-15 16:51:28 -0600

    Acorte los if del movimiento del personaje y agregue el sonido de pasos.

--- niveles.py
+++ niveles.py
@@ -19,12 +19,12 @@ class level:
         self.score = 0
         self.world_x_shift = 0
         self.world_y_shift = 0
-        
+
     # Se Itera una lista y se colocan sprites en el mapa según la posicion
     # de los caracteres encontrados
     def set_walls(self, layout, wall_type):
         self.im = Image.open(wall_type)
-        
+
         self.walls = pygame.sprite.Group()
         self.items = pygame.sprite.Group()
         self.door_keys = pygame.sprite.Group()
@@ -42,11 +42,11 @@ class level:
                     j = row_index*self.im.size[0]-(400)
                     k = col_index*self.im.size[1]-(300)
                     item = items((j, k))
-                    
+
                     self.items.add(item)
 
                 elif cell == "r": # Deteccion de pixeles rojos
-                    if len(self.door_keys) < 1: 
+                    if len(self.door_keys) < 1:
                         j = row_index*self.im.size[0]-(400)
                         k = col_index*self.im.size[1]-(300)
                         key = door_key((j, k))
@@ -60,8 +60,8 @@ class level:
                     door = lock_door((j, k))
                     self.lock_doors.add(door)
 
-                
-        
+
+
     # Posicion del jugador e la pantalla
     def set_player(self):
 
@@ -69,7 +69,7 @@ class level:
         self.player = ply((400, 300))
         self.player_list = pygame.sprite.Group()
         self.player_list.add(self.player)
-        
+
 
     # Movimiento del mundo y sonido
     def scroll_world(self):
@@ -81,11 +81,11 @@ class level:
             self.world_y_shift=0
 
         self.keys = pygame.key.get_pressed()
-        
-        
-        
+
+
+
         # Controles del jugador van aqui
-        if self.keys[pygame.K_a] and not self.keys[pygame.K_d] and not self.keys[pygame.K_w] and not self.keys[pygame.K_s]:
+        if self.keys[pygame.K_a] :
 
             if self.player.rect.x < 360:
                 self.player.speed =0
@@ -97,9 +97,9 @@ class level:
                 if self.keys[pygame.K_LSHIFT]:
                     self.player.rect.x -= 3
             return
-        elif self.keys[pygame.K_d] and not self.keys[pygame.K_a] and not self.keys[pygame.K_w] and not self.keys[pygame.K_s]:
- 
-            
+        elif self.keys[pygame.K_d] :
+
+
             if self.player.rect.x > 440:
                 self.player.speed =0
                 self.world_x_shift=-1
@@ -111,8 +111,8 @@ class level:
                     self.player.rect.x += 3
 
             return
-        elif self.keys[pygame.K_w] and not self.keys[pygame.K_d] and not self.keys[pygame.K_a] and not self.keys[pygame.K_s]:
- 
+        elif self.keys[pygame.K_w] :
+
             if self.player.rect.y < 260:
                 self.player.speed =0
                 self.world_y_shift=1
@@ -124,8 +124,8 @@ class level:
                     self.player.rect.y -= 3
 
             return
-        elif self.keys[pygame.K_s] and not self.keys[pygame.K_d] and not self.keys[pygame.K_w] and not self.keys[pygame.K_a]:
-  
+        elif self.keys[pygame.K_s]:
+
             if self.player.rect.y > 340:
                 self.player.speed =0
                 self.world_y_shift=-1
@@ -141,7 +141,7 @@ class level:
             self.world_y_shift=0
             return
 
-        
+
     def colisiones(self):
         for i in self.items:
             i.speed = 0
@@ -152,42 +152,42 @@ class level:
 
         for wall in self.walls:
             if wall.rect.colliderect(self.player):
-                
+
                 if self.keys[pygame.K_a]:
                     self.player.speed =0
                     self.world_x_shift -=10
-                    
+
 
                 if self.keys[pygame.K_d]:
                     self.player.speed =0
                     self.world_x_shift +=10
-                    
+
 
                 if self.keys[pygame.K_w]:
                     self.player.speed =0
                     self.world_y_shift -=10
-                    
+
 
                 if self.keys[pygame.K_s]:
                     self.player.speed =0
                     self.world_y_shift +=10
-    
+
     def puntaje(self):
 
-        
+
         for i in self.items:
-            
+
             if i.rect.colliderect(self.player):
                 self.items.remove(i)
                 self.score += 100
         return self.score
 
 
-        
-        
+
+
     # Activacion de todas las funciones de esta clase
     def run(self):
-        
+
         self.walls.update(self.world_x_shift, self.world_y_shift)
         self.walls.draw(self.diplay_surface)
         self.items.update(self.world_x_shift, self.world_y_shift)
@@ -200,8 +200,3 @@ class level:
         self.scroll_world()
         self.colisiones()
         self.puntaje()
-        
-
-
-
-
