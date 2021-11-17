4037224c16318bf0911ccdcb7db83d1876b03756
Alexander <ap16012001@gmail.com>
2021-11-16 18:34:00 -0600

    En linea 15 se agrego el ".convert()", para evitar lag con imagenes.

--- Mapas_pruebas.py
+++ Mapas_pruebas.py
@@ -11,12 +11,12 @@ def mapeo(image):
     que representa los pixeles negros de la imagen con la letra b
     """
 
-    im = Image.open(image) # Carga la imagen
-    
+    im = Image.open(image).convert() # Carga la imagen
+
     # Cambia el tamaño de la imagen para que se ajuste a la
     # pantalla del juego
-    im2 = im.resize((screen_size.current_h-150,screen_size.current_h-150)) 
-    
+    im2 = im.resize((screen_size.current_h-150,screen_size.current_h-150))
+
     # Crea una lista con todos los pixeles en RGB de a imagen
     pix = im2.load()
     mapa=[] # Lista que guarda la posicion de los ixeles relevantes de la imagen
@@ -25,10 +25,9 @@ def mapeo(image):
     for row in range(0,im2.size[0],5): # Revision de filas de pixeles de la imagen
         a=[]
         for column in range(0,im2.size[1],5): # Revision de columnas de pixeles de la imagen
-            
+
             # Comprobación de rango aceptable de tonos de negro
             if pix[row, column][0] in range(x) and pix[row, column][1] in range(x) and pix[row, column][2] in range(x):
-                
                 a.append("b")
 
             elif pix[row, column][0] in range(x) and pix[row, column][1] in range(255-x,255,1) and pix[row, column][2] in range(x):
@@ -41,10 +40,10 @@ def mapeo(image):
                 a.append("a")
             else:
                 a.append(" ")
-            
-    
-            
+
+
+
         b = "".join(a) # Definicion de str ue contiene 1 fila de pixeles de la imagen
-        mapa.append(b) 
-    
+        mapa.append(b)
+
     return mapa
