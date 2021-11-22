0944de02b45afd6de7a754667ff269b11b36a6fc
Alexander <ap16012001@gmail.com>
2021-11-22 01:01:50 -0600

    Se agregaron los identificadores de niveles.

--- config.py
+++ config.py
@@ -7,5 +7,10 @@ from PIL import Image
 level_map_1 = mapeo('Laberintos\Level_1.jpg')
 level_map_2 = mapeo('Laberintos\Level_2.jpg')
 level_map_3 = mapeo('Laberintos\Level_3.jpg')
-
-
+level_map_4 = mapeo('Laberintos\Level_4.jpg')
+level_map_5 = mapeo('Laberintos\Level_5.jpg')
+level_map_6 = mapeo('Laberintos\Level_6.jpg')
+level_map_7 = mapeo('Laberintos\Level_7.jpg')
+level_map_8 = mapeo('Laberintos\Level_8.jpg')
+level_map_9 = mapeo('Laberintos\Level_9.jpg')
+level_map_10 = mapeo('Laberintos\Level_10.jpg')
