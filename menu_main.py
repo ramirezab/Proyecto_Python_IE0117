0bc232eceb2535c22b4a969ea52220edd4d568de
jodadiz <jodadi04@gmail.com>
2021-11-24 15:19:34 -0600

    Conexion entre main y menu

--- menu_main.py
+++ menu_main.py
@@ -1,15 +1,20 @@
 import pygame
+import sys
 import time
+from pygame.constants import FULLSCREEN
+from config import *
+from niveles import level
+from pygame import mixer
 from pygame.locals import *
 
 #Constantes para el codigo
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 pygame.display.set_caption('Labyrinth quest')
 
-background_menu = pygame.image.load('Fondos/prueba.JPG').convert()
+#tamaño de la pantalla
+screen = pygame.display.set_mode((800, 600))
+
+background_menu = pygame.image.load('Fondos\prueba.jpg').convert()
 
 #Colores
 black = (0,0,0)
@@ -22,13 +27,63 @@ yellow = (255,255,0)
 
 #Inicializacion
 pygame.init()
+pygame.mixer.init()
+pygame.font.init()
+
+# Detecta la informacion de la pantalla
+display_info = pygame.display.Info()
+screen_width = display_info.current_w
+screen_height = display_info.current_h
+
+# Creación de niveles
+level_1 = level(level_map_1, screen, "sprites\Sprite lvl 1.png")
+level_2 = level(level_map_2, screen, "sprites\Sprite lvl 2.png")
+level_3 = level(level_map_3, screen, "sprites\Sprite lvl 3.png")
+level_4 = level(level_map_4, screen ,"sprites\Sprite lvl 4.png")
+level_5 = level(level_map_5, screen ,"sprites\Sprite lvl 5.png")
+level_6 = level(level_map_6, screen ,"sprites\Sprite lvl 6.png")
+level_7 = level(level_map_7, screen ,"sprites\Sprite lvl 7.png")
+level_8 = level(level_map_8, screen ,"sprites\Sprite lvl 8.png")
+level_9 = level(level_map_9, screen ,"sprites\Sprite lvl 9.png")
+level_10 = level(level_map_10, screen ,"sprites\Sprite lvl 10.png")
+
+# Con esto se puede identificar la musica, los pasos de cada nivel
+# Y elegir el nivel mismo
+nivel = level_2
+
+if (nivel == level_1):
+    cancion = "Sonidos\Musica playa.mp3"
+elif(nivel == level_2):
+    cancion = "Sonidos\Musica bosque.mp3"
+elif(nivel == level_3):
+    cancion = "Sonidos\Musica selva.mp3"
+elif(nivel == level_4):
+    cancion = "Sonidos\Musica nieve.mp3"
+elif (nivel == level_5):
+    cancion = "Sonidos\Musica mazmorra.mp3"
+elif (nivel == level_6):
+    cancion = "Sonidos\Musica dungeon.mp3"
+elif (nivel == level_7):
+    cancion = "Sonidos\Musica luna.mp3"
+elif (nivel == level_8):
+    cancion = "Sonidos\Musica inferno.mp3"
+elif (nivel == level_9):
+    cancion = "Sonidos\Musica marte.mp3"
+elif (nivel == level_10):
+    cancion = "Sonidos\Musica cielo.mp3"
+
+#pasos
+if (nivel == level_2):
+    pasos = "Sonidos\Pasos en arena.mp3"
+elif (nivel == level_1):
+    pasos = "Sonidos\Pasos metal.mp3"
 
 #Fuentes para la letra de menu
 small_font = pygame.font.SysFont('comicsansms',15)
 medium_font = pygame.font.SysFont('comicsansms',30)
 large_font = pygame.font.SysFont('comicsansms',50)
 
-#Datos para los botones en menu
+#Datos para los botones en menu principal
 size_button = [200,45]
 
 button_levels = [300,200]
@@ -42,6 +97,9 @@ color_controls = [plomo,green]
 
 button_exit = [300,410]
 color_exit = [plomo,yellow]
+
+button_regress = [625,500]
+color_regress = [plomo,yellow]
 
 #Datos para botones de niveles
 size_but_lvl = [100,45]
@@ -70,7 +128,7 @@ def buttons(text,surface,state,posit,size_butt,id_button = None):
     cursor = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
 
-#Define si el cursor esta en el boton o no
+    #Define si el cursor esta en el boton o no
     if posit[0] + size_butt[0] > cursor[0] > size_butt[0] and posit[0] + size_butt[0] < cursor[0] + size_butt[0] and posit[1] + size_butt[1] > cursor[1] > size_butt[1] and posit[1] + size_butt[1] < cursor[1] + size_butt[1]:
 
         if click[0] == 1:
@@ -80,9 +138,34 @@ def buttons(text,surface,state,posit,size_butt,id_button = None):
                 options()
             elif id_button == 'detalles':
                 controls()
+            elif id_button == 'regresar':
+                intromenu()
             elif id_button == 'salir':
                 pygame.quit()
                 quit()
+
+            #Para los niveles
+            elif id_button == 'lvl1':
+                level_1.run()
+
+            elif id_button == 'lvl2':
+                level_2.run()
+            elif id_button == 'lvl3':
+                level_3.run()
+            elif id_button == 'lvl4':
+                level_4.run()
+            elif id_button == 'lvl5':
+                level_5.run()
+            elif id_button == 'lvl6':
+                level_6.run()
+            elif id_button == 'lvl7':
+                level_7.run()
+            elif id_button == 'lvl8':
+                level_8.run()
+            elif id_button == 'lvl9':
+                level_9.run()
+            elif id_button == 'lvl10':
+                level_10.run()
 
         button = pygame.draw.rect(surface,state[1],(posit[0],posit[1],size_butt[0],size_butt[1]))
     else:
@@ -110,7 +193,6 @@ def message(msg,color,despla_Y=0,size='small'):
 #Menu principal, primera cara del juego
 def intromenu():
     introgame = True
-
     while introgame:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -119,16 +201,16 @@ def intromenu():
 
         screen.blit(background_menu,[0,0])
 
-#Botones y sus textos en la pagina menu
+        # Botones y sus textos en la pagina menu
         buttons('NIVELES',screen,color_levels,button_levels,size_button,id_button='niveles')
         buttons('OPCIONES',screen,color_options,button_options,size_button,id_button='configuracion')
         buttons('CONTROLES',screen,color_controls,button_controls,size_button,id_button='detalles')
         buttons('SALIR',screen,color_exit,button_exit,size_button,id_button='salir')
 
-#Mensajes libres en la pantalla
+        #Mensajes libres en la pantalla
         message('LABYRINTH QUEST',black,-250,size='large')
         message('LABYRINTH QUEST',red,-246,size='large')
-        message('Presionar X para ir hacia atras',white,200,size='medium')
+
         pygame.display.update()
         clock.tick(60)
 
@@ -148,6 +230,8 @@ def options():
                     regres = False
 
             screen.fill(white)
+
+            buttons('ATRAS',screen,color_regress,button_regress,size_but_lvl,id_button='regresar')
             message('Aqui iran las opciones',red,-100,size='medium')
             pygame.display.update()
             clock.tick(60)
@@ -167,6 +251,8 @@ def controls():
                     regres = False
 
             screen.fill(white)
+
+            buttons('ATRAS',screen,color_regress,button_regress,size_but_lvl,id_button='regresar')
             message('Aqui iran los controles',red,-100,size='medium')
             pygame.display.update()
             clock.tick(60)
@@ -201,21 +287,68 @@ def menulevels():
         buttons('Nivel IX',screen,color_but_lvl,button_lvl9,size_but_lvl,id_button='lvl9')
         buttons('Nivel X',screen,color_but_lvl,button_lvl10,size_but_lvl,id_button='lvl10')
 
+        buttons('ATRAS',screen,color_regress,button_regress,size_but_lvl,id_button='regresar')
 
-#Ejecucion de los diferentes niveles
-def gameloop():
-    exit = False
+# Muestra FPS
+class FPS:
+    def __init__ (self, puntaje):
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.SysFont("Comic Sans MS", 20)
+        self.text = self.font.render(str(puntaje), True, (0,0,0))
+        screen.blit(self.text, (10,10))
+    def render(self, screen, puntaje):
+        self.text = self.font.render(str(puntaje), True, (0,0,0))
+        screen.blit(self.text, (10,10))
+fps = FPS(level_1.puntaje())
 
-    while not exit:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+# Bucle principal del juego
+while True:
+    for event in pygame.event.get():
+        # Inicio del modulo de sonido 1
+        if event.type == pygame.KEYDOWN:
+            if (event.key == pygame.K_LEFT or event.key == pygame.K_a):
+                Pasos.play(-1)
+            elif (event.key == pygame.K_RIGHT or event.key == pygame.K_d):
+                Pasos.play(-1)
+            elif (event.key == pygame.K_DOWN or event.key == pygame.K_s):
+                Pasos.play(-1)
+            elif (event.key == pygame.K_UP or event.key == pygame.K_w):
+                Pasos.play(-1)
+                # Con este modulo si se apreta m se pausa la musica
+                # Con n se reanuda donde se dejo pausada
+            elif (event.key == pygame.K_m):
+                pygame.mixer.music.pause()
+            elif (event.key == pygame.K_n):
+                pygame.mixer.music.unpause()
+        # Fin del modulo de sonido
+
+        # Con este comando si apretas ESC el juego se cerrará
+            if event.key == pygame.K_ESCAPE:
                 pygame.quit()
-                exit = True
-            screen.fill(white)
-            message('Ejecucion de los diferentes niveles',red,-100,size='medium')
-            pygame.display.update()
-            clock.tick(60)
+                sys.exit()
 
-intromenu()
-quit()
-gameloop()
+        # Inicio modulo de sonido 2
+        if (event.type == pygame.KEYUP):
+            if (event.key == pygame.K_LEFT or event.key == pygame.K_a):
+                Pasos.stop()
+            elif (event.key == pygame.K_RIGHT or event.key == pygame.K_d):
+                Pasos.stop()
+            elif (event.key == pygame.K_DOWN or event.key == pygame.K_s):
+                Pasos.stop()
+            elif (event.key == pygame.K_UP or event.key == pygame.K_w):
+                Pasos.stop()
+        # Fin modulo de sonido 2
+
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+
+    screen.fill('grey')
+
+    intromenu()
+    menulevels()
+    #level_10.run()
+
+    fps.render(screen, level_10.puntaje())
+    fps.clock.tick(60)
+    pygame.display.flip()
