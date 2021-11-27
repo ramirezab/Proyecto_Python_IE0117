bdeb32849bebd3827900c602be92bdd7101ce6c3
jodadiz <jodadi04@gmail.com>
2021-11-26 20:04:09 -0600

    Menu de pausa y clase de sprite final

--- sprites.py
+++ sprites.py
@@ -6,9 +6,9 @@ class wall(pygame.sprite.Sprite):
     def __init__(self, pos, sprite):
         super().__init__()
         self.image = pygame.image.load(sprite).convert()
-        
+
         self.rect = self.image.get_rect(topleft = pos)
-    
+
     def update(self, x_shif, y_shift): # Posicion de los sprites para su movimiento
         self.rect.x += x_shif
         self.rect.y += y_shift
@@ -37,7 +37,7 @@ class lock_door(pygame.sprite.Sprite):
 
         self.rect.x += x_shif
         self.rect.y += y_shift
-        
+
 
 class door_key(pygame.sprite.Sprite):
     def __init__(self, pos):
@@ -45,10 +45,28 @@ class door_key(pygame.sprite.Sprite):
         self.image = pygame.image.load("sprites\key.png").convert()
         self.image.set_colorkey((0, 0, 0))
         self.rect = self.image.get_rect(topleft = pos)
-    
+
     def update(self, x_shif, y_shift):
 
         self.rect.x += x_shif
         self.rect.y += y_shift
 
+# class finish(pygame.sprite.Sprite):
+#     def __init__(self, pos):
+#         super().__init__()
+#         self.image = pygame.image.load("sprites\key.png").convert()
+#         self.image.set_colorkey((0, 0, 0))
+#         self.rect = self.image.get_rect(topleft = pos)
+#
+#     def update(self, x_shif, y_shift):
+#
+#         self.rect.x += x_shif
+#         self.rect.y += y_shift
 
+
+#Alpha para dibujar cuadros trasparentes, funcion todavia no en uso
+
+    # def draw_rect_alpha(self, surface, color, rect):
+    # shape_surf = pygame.Surface(pygame.Rect(rect).size, pygame.SRCALPHA)
+    # pygame.draw.rect(shape_surf, color, shape_surf.get_rect())
+    # surface.blit(shape_surf, rect)
